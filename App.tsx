7e090357f799e3b092fc79b4711359e33acec58d
Tom Riglar <tom@riglar.com>
2020-09-20 18:00:17 +0100

    upgrade to sdk 38

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { Routing } from "./src/navigation";
+import { initialiseOtaManager } from "expo-ota-manager";
 
 export default function App() {
+  useEffect(() => {
+    initialiseOtaManager({
+      noButtonText: "Not Now",
+      yesButtonText: "Restart and Update",
+      textLines: ["Would you like to restart to start using it?"],
+      titleText: "An update is available",
+      repromptIntervalMs: 1000,
+    });
+  }, []);
+
   return (
     <NavigationContainer>
       <Routing />
